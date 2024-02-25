8086bd3860118d9e8f9c090f706cfbefd2644f44
Gibron Khan <khangibron@gmail.com>
2024-02-25 01:41:36 -0500

    click rock to move

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public GameObject rock;
+
+    //keyboard movement
     private Vector2 movementDirection;
     [SerializeField] private float movementSpeed = 2f;
-    // Start is called before the first frame update
+
+    //grapple movement
+    [SerializeField] private float forceAmount = 10f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,6 +24,19 @@ public class movement : MonoBehaviour
     void Update()
     {
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D click = Physics2D.Raycast(mousePosition, Vector2.zero);
+            if (click.collider != null && click.collider.gameObject.name == "rock")
+            {
+                //Debug.Log("Rock was clicked");
+                Vector2 direction = (rock.transform.position - rb.transform.position).normalized;
+                //Debug.Log(direction);
+                rb.AddForce(direction * forceAmount);
+            }
+        }
     }
 
     private void FixedUpdate()
