f26719257dc4eea809ecfe52475074f463e08336
Gibron Khan <khangibron@gmail.com>
2024-02-25 00:39:57 -0500

    THE THING MOVES

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+    [SerializeField] private float movementSpeed = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movementDirection * movementSpeed;
     }
 }
